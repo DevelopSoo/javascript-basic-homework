e78ff6504660bc357089dd36ef369717c6b2f1b1
DevelopSoo <69490984+DevelopSoo@users.noreply.github.com>
2023-11-09 10:34:44 +0900

    update: 3회차 과제 세팅

---
+++ 12번/12.js
@@ -0,0 +1,14 @@
+const 투두 = {
+  id: 1,
+  할일: "8시기상",
+  완료여부: false,
+};
+
+// 여기에 코드를 작성해주세요.
+const newTodo = ?
+
+console.log(투두)
+console.log(newTodo);
+// 결과
+// { id: 1, '할일': '8시기상', '완료여부': false }
+// { id: 1, '할일': '8시기상', '완료여부': true }
