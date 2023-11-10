36d7729edcd29525cd247ec55e0f7143c3eecb1d
gilhun <qkrrlfgns123@gmail.com>
2023-11-10 16:34:00 +0900

    3회차 과제 완료

--- 3rd/12번/12.js
+++ 3rd/12번/12.js
@@ -5,9 +5,9 @@ const 투두 = {
 };
 
 // 여기에 코드를 작성해주세요.
-const newTodo = ?
+const newTodo = { ...투두, 완료여부: true };
 
-console.log(투두)
+console.log(투두);
 console.log(newTodo);
 // 결과
 // { id: 1, '할일': '8시기상', '완료여부': false }
